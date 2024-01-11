47bf75f5ab3cd8fda9c9f3be014c5842441760e5
Tyco266 <tyco.opilski@gmail.com>
2024-01-11 21:16:58 +0100

    Initialisieren des Spielfeldes (initializePlayGround) Anzeigen des Spielfeldes (showPlayGround)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Arrays;
+
 public class Main {
+
+    private static void initializePlayGround(char[][] playGround) {
+        for (char[] row : playGround) {
+            Arrays.fill(row, '~');
+        }
+    }
+
+    private static void showPlayGround(char[][] playGround, int size) {
+        System.out.println("  0 1 2 3 4");
+        for (int i = 0; i < size; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < playGround[i].length; j++) {
+                System.out.print(playGround[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
     public static void main(String[] args) {
-        char[][] playGround = new char[5][5];
+        int size = 5;
+        char[][] playGround = new char[size][size];
+
+        initializePlayGround(playGround);
+        showPlayGround(playGround, size);
+
     }
 }
