68163d07c0026f4748ebf46bf08ee55f6b848a53
Tyco266 <tyco.opilski@gmail.com>
2024-01-14 01:58:18 +0100

    Auswahl der Spielfeldgröße nun möglich.

--- src/Main.java
+++ src/Main.java
@@ -12,8 +12,32 @@ public class Main {
         while (!Objects.equals(start, "Start")) {
             start = scanner.nextLine();
         }
+    }
+
+    private static int gameSize() {
+        int size;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println("Bitte größe des Spielfelds als einzelne Zahl eingeben.");
+                size = scanner.nextInt();
+                if (size > 3) {
+                    break;
+                } else {
+                    System.out.println("Die Größe muss größer als 0 sein.S");
+                    System.out.println();
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.");
+                System.out.println();
+                scanner.next();
+            }
+        }
+
+
         System.out.println("Das Spiel wird gestartet");
         System.out.println();
+        return size;
     }
 
     private static void initializePlayGround(char[][] playGround) {
@@ -40,10 +64,11 @@ public class Main {
 
 
     public static void main(String[] args) {
-        int size = 5;
+        gameStart();
+
+        int size = gameSize();
         char[][] playGround = new char[size][size];
 
-        gameStart();
         initializePlayGround(playGround);
         showPlayGround(playGround, size);
 
