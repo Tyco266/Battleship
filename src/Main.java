1f10a1bfbab6f3dedeb51021e06e79d41fa80104
Tyco266 <tyco.opilski@gmail.com>
2024-01-15 11:10:05 +0100

    Optimierung

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,55 @@
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
 
+    public static void main(String[] args) {
+        gameStart();
+
+        int size = gameSize();
+        char[][] playGround = new char[size][size];
+
+        initializePlayGround(playGround);
+        showPlayGround(playGround, size);
+
+    }
 
     private static void gameStart() {
         Scanner scanner = new Scanner(System.in);
-        String choose;
 
         while (true) {
             System.out.println("Zum starten des Spiels 'Start' eingeben, zum lesen der Anleitung 'Anleitung' eingeben.");
-            choose = scanner.nextLine();
+            String choose = scanner.nextLine();
 
             if (Objects.equals(choose, "Anleitung")) {
-                System.out.println("Anleitung hier");
-                System.out.println();
+                System.out.println("Anleitung hier\n");
                 continue;
             } else if (Objects.equals(choose, "Start")) {
                 System.out.println("Spiel gestartet");
                 break;
-            } else {
-                System.out.println("Ungültige Eingabe. Bitte 'Start' oder 'Anleitung' eingeben.");
             }
+            System.out.println("Ungültige Eingabe. Bitte 'Start' oder 'Anleitung' eingeben.");
+
         }
 
     }
 
     private static int gameSize() {
-        int size;
         Scanner scanner = new Scanner(System.in);
         while (true) {
             try {
                 System.out.println("Bitte größe des Spielfelds als einzelne Zahl eingeben.");
-                size = scanner.nextInt();
+                int size = scanner.nextInt();
                 if (size > 3) {
-                    break;
-                } else {
-                    System.out.println("Die Größe muss größer als 0 sein.S");
+                    System.out.println("Das Spiel wird gestartet");
                     System.out.println();
+                    return size;
+                } else {
+                    System.out.println("Die Größe muss größer als 0 sein.\n");
                 }
-            } catch (java.util.InputMismatchException e) {
+            } catch (InputMismatchException e) {
                 System.out.println("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.");
                 System.out.println();
                 scanner.next();
@@ -48,9 +57,6 @@ public class Main {
         }
 
 
-        System.out.println("Das Spiel wird gestartet");
-        System.out.println();
-        return size;
     }
 
     private static void initializePlayGround(char[][] playGround) {
@@ -62,28 +68,18 @@ public class Main {
     private static void showPlayGround(char[][] playGround, int size) {
         System.out.print("   ");
         for (int i = 0; i < size; i++) {
-            System.out.print(i + "  ");
+            System.out.printf("%2d  ",i);
+            //System.out.print(i + "  ");
         }
         System.out.println();
         for (int i = 0; i < size; i++) {
-            System.out.print(i + "  ");
+            System.out.printf("%2d  ",i);
+            //System.out.print(i + "  ");
             for (int j = 0; j < size; j++) {
-                System.out.print(playGround[i][j] + "  ");
+                System.out.print(playGround[i][j] + " ".repeat(size >= 10 ? 3 : 2));
             }
             System.out.println();
         }
         System.out.println();
-    }
-
-
-    public static void main(String[] args) {
-        gameStart();
-
-        int size = gameSize();
-        char[][] playGround = new char[size][size];
-
-        initializePlayGround(playGround);
-        showPlayGround(playGround, size);
-
     }
 }
